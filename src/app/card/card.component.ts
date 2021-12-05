0c29327e8a23f33d234c21beec4e398d796a6422
Benoît Hubert <benoithubert@gmail.com>
2021-12-05 22:23:15 +0100

    Slide 62c - declare class Post and use it to type post input

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+// La classe permet de décrire les attributs
+// de l'objet `post` passé depuis le parent
+class Post {
+  title!: string;
+  description!: string;
+  picture!: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  @Input() post!: any;
+  @Input() post!: Post;
 
   constructor() { }
 
