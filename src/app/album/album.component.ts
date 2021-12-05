5e1255a991f050bcc221a09c4314460bb17378a2
Benoît Hubert <benoithubert@gmail.com>
2021-12-05 22:29:32 +0100

    Slides 70-72 - move post2's data to posts array

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -11,13 +11,13 @@ export class AlbumComponent implements OnInit {
       picture: 'https://unsplash.com/photos/JH0wCegJsrQ/download?force=true&w=640',
       title: 'La nuit',
       description: "C'est beau une ville la nuit !"
+    },
+    {
+      picture: 'https://unsplash.com/photos/1kf69eE7VR4/download?force=true&w=640',
+      title: 'San Francisco',
+      description: "Le Golden Gate !"
     }
   ];
-  post2 = {
-    picture: 'https://unsplash.com/photos/1kf69eE7VR4/download?force=true&w=640',
-    title: 'San Francisco',
-    description: "Le Golden Gate !"
-  };
 
   constructor() { }
 
