b6d63e7349550da5d3797db12e3a02d85b08477f
Benoît Hubert <benoithubert@gmail.com>
2021-12-05 21:36:51 +0100

    Slides 38-41 - Add post1 attribute to AlbumComponent, display title and description

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album.component.css']
 })
 export class AlbumComponent implements OnInit {
+  post1 = {
+    picture: 'https://unsplash.com/photos/JH0wCegJsrQ/download?force=true&w=640',
+    title: 'La nuit',
+    description: "C'est beau une ville la nuit !"
+  }
 
   constructor() { }
 
