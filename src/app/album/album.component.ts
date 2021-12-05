77c4a57c32758693acee4e2ed3090abe4d99b2ad
Benoît Hubert <benoithubert@gmail.com>
2021-12-05 21:44:51 +0100

    Slides 47-49 - Display 2nd post data in 2nd album card

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -10,7 +10,12 @@ export class AlbumComponent implements OnInit {
     picture: 'https://unsplash.com/photos/JH0wCegJsrQ/download?force=true&w=640',
     title: 'La nuit',
     description: "C'est beau une ville la nuit !"
-  }
+  };
+  post2 = {
+    picture: 'https://unsplash.com/photos/1kf69eE7VR4/download?force=true&w=640',
+    title: 'San Francisco',
+    description: "Le Golden Gate !"
+  };
 
   constructor() { }
 
