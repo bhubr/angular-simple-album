10695b7c74a773d246e720b2be62693b15ef5e14
Benoît Hubert <benoithubert@gmail.com>
2021-12-05 20:59:56 +0100

    Slide 17 - generate album component and add content to template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule
