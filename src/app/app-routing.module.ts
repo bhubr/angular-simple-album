b603bf0e00ea4311bdf6aa54135d4395615888bc
Benoît Hubert <benoithubert@gmail.com>
2021-12-10 02:52:24 +0100

    Slides 103-104 - naive way to route to details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AlbumComponent } from './album/album.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: '', component: AlbumComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'details', component: DetailsComponent },
 ];
 
 @NgModule({
